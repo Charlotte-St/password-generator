b34aa0bdd8f2723600c9fb0393c0903f48605ed0
Lotte-Et <charlotte.stowe@gmail.com>
2023-12-30 16:47:40 -0500

    Updates to function to add additional characters

--- Develop/script.js
+++ Develop/script.js
@@ -53,12 +53,13 @@ function specialChoice(){
 
   // Functions to assemble password
   function additionalChars() {
-    for (let i = 0; i < additionalCharLength; i++){
-      var randomChar = passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)];
+   for (let i = passwordComponents.length; i < passwordLength; i++){
+     var randomChar = passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)];
       console.log(randomChar);
       return passwordComponents.push(randomChar);
-    }
-  }
+    };
+  };
+  //};
 
   // confirm generate password
 if (confirm("Would you like to generate a password?")==true){
@@ -78,24 +79,24 @@ if (confirm("Would you like to generate a password?")==true){
   upperCaseChoice();
   numberChoice();
   specialChoice();
-  var additionalCharLength = passwordLength - passwordComponents.length;
+  console.log(passwordCharSet.toString());
+  console.log(passwordComponents.length);
+  var additionalCharLength = (passwordLength - passwordComponents.length);
   console.log(additionalCharLength);
   additionalChars();
+
+  //let i = 0;
+  //for(i; i <= additionalCharLength; i++){
+    //var randomChar = passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)];
+      //console.log(randomChar)
+      //return passwordComponents.push(randomChar);
+};
+
   var assembledPassword = passwordComponents.toString();
   alert(assembledPassword);
 }
 else {
  alert("You have chosen not to create a password.")
-};
-  // select password length
-
-  // confirm use lower case char --> add random character from lowerCase to passwordComponents; add lowerCase to passwordCharSet
-// confirm use upper case char --> add random character from upperCase to passwordComponents; add upperCase to passwordCharSet
-// confirm use numbers --> add random character from numberLost to passwordComponents; add numberList to passwordCharSet
-//confirm use special characters (provide list of special char)--> add random character from specialChar to passwordComponents; add specialChar to passwordCharSet 
-// error if no to lower, upper, numbers, and special chars --> break and have user restart
-// add additional characters to password --> iterate over loop to add random character from array to passwordComponents
-// iterate over loop to concatenate values from passwordComponents into password and return value; Randomize before concatenating? 
 };
 
 // Write password to the #password input
