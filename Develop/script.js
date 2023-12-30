edcf11fc32f075eb1af610af2a1231d886eb0728
Lotte-Et <charlotte.stowe@gmail.com>
2023-12-30 10:41:22 -0500

    Added prompts and checking for password length

--- Develop/script.js
+++ Develop/script.js
@@ -9,15 +9,27 @@ function generatePassword(){
   var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
   var numberList = ['0','1','2','3','4','5','6','7','8','9'];
   var specialChar = ['!','#','$','%','&','(',')','*','+','-','/','<','=','>','?','@','[',']','{','}','|'];
+  var passwordLength = 0;
   var passwordCharSet = [];
   var passwordComponents = [];
 
+//Functions to select character types
+function lowerCaseChoice(){
+  if (confirm("Should the password include lower case letters?")==true) {
+    passwordCharSet.push(lowerCase);
+  }
+};
+
   // confirm generate password
 if (confirm("Would you like to generate a password?")==true){
-  alert("you pressed OK")
+  let passwordLength = Number((prompt("How long should the password be?\n Min: 8 characters\n Max: 128 characters")))
+  if (passwordLength < 8 || passwordLength > 128){
+    alert("The password must be between 8 and 128 characters. Please restart the password generation process.")
+  } 
+  else {alert( passwordLength + " character password requested.")}
 }
 else {
- alert("you pressed no")
+ alert("You have chosen not to create a password.")
 };
   // select password length
 
