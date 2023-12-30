6a58a477e670191f2b00d7cc89b5782d7f3df76e
Lotte-Et <charlotte.stowe@gmail.com>
2023-12-29 20:25:26 -0500

    Started to add alert boxes

--- Develop/script.js
+++ Develop/script.js
@@ -4,8 +4,11 @@ var generateBtn = document.querySelector("#generate");
 //  Password generation
 
 function generatePassword(){
-  
-};
+  if (window.confirm("Would you like to generate a password?")){
+    text1 = "Follow the prompts to generate a password"
+  }
+  else {text1 = "You pressed 'Cancel'./n Press the 'Generate Password' button to restart the process."}
+  };
 
 // Write password to the #password input
 function writePassword() {
